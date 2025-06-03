1a98f956f270b4048d37e2d5f8eaca28a114192b
JAHENAO04 <juanhenao324869@correo.itm.edu.co>
2025-06-02 23:47:49 -0500

    ciudad-cliente

---
+++ Paginas/Scripts/Ciudad.js
@@ -0,0 +1,44 @@
+﻿var BaseURL = "http://inmobiliaria.runasp.net/";
+
+jQuery(function () {
+    $("#dvMenu").load("../Paginas/Menu.html");
+    LlenarTablaCiudades();
+});
+
+function LlenarTablaCiudades() {
+    let URL = BaseURL + "api/Ciudades/ConsultarTodos";
+    LlenarTablaXServiciosAuth(URL, "#tblCiudades");
+}
+
+async function EjecutarComando(Metodo, Funcion) {
+    let URL = BaseURL + "api/Ciudades/" + Funcion;
+    // Construye el objeto ciudad
+    const ciudad = new Ciudad($("#txtid_ciudad").val(), $("#txtnombre").val(), $("#txtdepartamento").val());
+    // Invoca el comando
+    const Rpta = await EjecutarComandoServicioRptaAuth(Metodo, URL, ciudad);
+    LlenarTablaCiudades();
+}
+
+async function Consultar() {
+    let nombre = $("#txtnombre").val();
+    let URL = BaseURL + "api/Ciudades/ConsultarXNombre?nombre=" + nombre;
+    const ciudad = await ConsultarServicioAuth(URL);
+    if (ciudad != null) {
+        $("#txtid_ciudad").val(ciudad.id_ciudad);
+        $("#txtnombre").val(ciudad.nombre);
+        $("#txtdepartamento").val(ciudad.departamento);
+    } else {
+        $("#dvMensaje").html("La ciudad no está en la base de datos");
+        $("#txtid_ciudad").val("");
+        $("#txtnombre").val("");
+        $("#txtdepartamento").val("");
+    }
+}
+
+class Ciudad {
+    constructor(id_ciudad, nombre, departamento) {
+        this.id_ciudad = id_ciudad;
+        this.nombre = nombre;
+        this.departamento = departamento;
+    }
+}
---
+++ Paginas/Scripts/Cliente.js
@@ -0,0 +1,73 @@
+﻿var BaseURL = "http://inmobiliaria.runasp.net/";
+
+jQuery(function () {
+    $("#dvMenu").load("../Paginas/Menu.html");
+    LlenarTablaClientes();
+});
+
+function LlenarTablaClientes() {
+    let URL = BaseURL + "api/Clientes/ConsultarTodos";
+    LlenarTablaXServiciosAuth(URL, "#tblClientes");
+}
+
+async function EjecutarComando(Metodo, Funcion) {
+    let URL = BaseURL + "api/Clientes/" + Funcion;
+
+    // Construir el objeto cliente
+    const cliente = new Cliente(
+        $("#txtid_cliente").val(),
+        $("#txtidentificacion").val(),
+        $("#txtnombres").val(),
+        $("#txtapellidos").val(),
+        $("#txttelefono").val(),
+        $("#txtemail").val(),
+        $("#txtdireccion").val(),
+        $("#txtfecha_registro").val()
+    );
+
+    // Ejecutar el comando
+    const Rpta = await EjecutarComandoServicioRptaAuth(Metodo, URL, cliente);
+    LlenarTablaClientes();
+}
+
+async function Consultar() {
+    let identificacion = $("#txtidentificacion").val();
+    let URL = BaseURL + "api/Clientes/ConsultarXDocumento?identificacion=" + identificacion;
+
+    const cliente = await ConsultarServicioAuth(URL);
+    if (cliente != null) {
+        $("#txtid_cliente").val(cliente.id_cliente);
+        $("#txtidentificacion").val(cliente.identificacion);
+        $("#txtnombres").val(cliente.nombres);
+        $("#txtapellidos").val(cliente.apellidos);
+        $("#txttelefono").val(cliente.telefono);
+        $("#txtemail").val(cliente.email);
+        $("#txtdireccion").val(cliente.direccion);
+        $("#txtfecha_registro").val(cliente.fecha_registro);
+        $("#dvMensaje").html("");
+    } else {
+        $("#dvMensaje").html("<div class='alert alert-warning'>El cliente no está en la base de datos</div>");
+        $("#txtid_cliente").val("");
+        $("#txtidentificacion").val("");
+        $("#txtnombres").val("");
+        $("#txtapellidos").val("");
+        $("#txttelefono").val("");
+        $("#txtemail").val("");
+        $("#txtdireccion").val("");
+        $("#txtfecha_registro").val("");
+    }
+
+}
+
+class Cliente {
+    constructor(id_cliente, identificacion, nombres, apellidos, telefono, email, direccion, fecha_registro) {
+        this.id_cliente = id_cliente;
+        this.identificacion = identificacion;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+        this.fecha_registro = fecha_registro;
+    }
+}
